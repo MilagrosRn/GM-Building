6de7d8263320eba459be00893d40015c4dc27fe9
MilagrosRn <milinr29@gmail.com>
2023-03-13 18:16:50 -0500

    add sass config and bar navigation component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ViewHomeComponent } from './components/templates/view-home/view-home.component'
+import { AssessorComponent } from './views/assessor/assessor.component';
+import { NavigationBarComponent } from './components/organisms/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/organisms/footer/footer.component';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewHomeComponent,
+    AssessorComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ my-app/src/app/views/assessor/assessor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assessor',
+  templateUrl: './assessor.component.html',
+  styleUrls: ['./assessor.component.scss']
+})
+export class AssessorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
