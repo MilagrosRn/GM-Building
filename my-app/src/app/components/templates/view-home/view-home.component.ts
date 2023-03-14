6285328d0f9427c39bcde85421bdbd4d614f5fb4
MilagrosRn <milinr29@gmail.com>
2023-03-14 15:39:51 -0500

    view-home v.01

--- my-app/src/app/components/templates/view-home/view-home.component.ts
+++ my-app/src/app/components/templates/view-home/view-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CAROUSEL_ITEMS } from 'src/app/models/constants/carousel.const';
+import { CarouselItem} from '../../../models/interfaces/carousel-item'
 
 @Component({
   selector: 'app-view-home',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewHomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public items: CarouselItem[] = CAROUSEL_ITEMS
+  public currentPosition = 0;
+  
+  constructor() { 
   }
 
+  ngOnInit(): void {
+    this.itemsCarouselVerifyData();
+    setTimeout(() => {this.setNext()}, 1500);
+  }
+
+  itemsCarouselVerifyData(){
+    this.items.map((element,index)=>{
+      element.id = index;
+      element.marginLeft = 0;
+    })
+  }
+
+  findCarouselItem():CarouselItem{
+    let findPositionByItem : CarouselItem | any = this.items.find((element:CarouselItem) => element.id === 0)
+    return findPositionByItem
+  }
+  setCarouselCurrentPosition(position:number){
+    this.currentPosition=position;
+    
+    if(this.findCarouselItem()){
+      this.findCarouselItem().marginLeft = -100 * position;
+    }
+  }
+
+  setNext() {
+    
+    let finalPercentage = 0;
+    let nextPosition = this.currentPosition + 1;
+    if(nextPosition <= this.items.length - 1){
+      finalPercentage = -100 * nextPosition;
+    } else {
+      nextPosition=0;
+    }
+   
+    if(this.findCarouselItem()){
+      this.findCarouselItem().marginLeft = finalPercentage;
+      this.currentPosition = nextPosition;
+    }
+    
+  }
+
+  setBack(){
+    let finalPercentage = 0;
+    let backPosition = this.currentPosition -1 ;
+    if(backPosition>=0){
+      finalPercentage = -100* backPosition;
+    }else{
+      backPosition=this.items.length - 1;
+      finalPercentage = -100 * backPosition;
+    }
+
+    if(this.findCarouselItem()){
+      this.findCarouselItem().marginLeft = backPosition;
+      this.currentPosition = backPosition;
+    }
+  }
 }
